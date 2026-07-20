546cdeeddfa829876fb425e410685542fedaf4e7
Alejandra05gsr <79392781+Alejandra05gsr@users.noreply.github.com>
2026-07-20 09:44:31 -0600

    Mejoras del raycast e instancias de objetos - Avances del 20 de Junio del 2026

--- Assets/_StickyKitchen/Scripts/Player/Shoot.cs
+++ Assets/_StickyKitchen/Scripts/Player/Shoot.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEngine.UI.Image;
 
 public class Shoot : MonoBehaviour
 {
     public float raycastDistance;
     public LayerMask layerMask;
+    public LayerMask layerMask1;
+    public LayerMask layerMask2;
     public Transform gunTransform;
+
+    public GameObject fluidObjectType1;
+    public GameObject fluidObjectType2;
 
     // Update is called once per frame
     void Update()
@@ -21,8 +27,21 @@ public class Shoot : MonoBehaviour
 
         if (Physics.Raycast(origin, direction, out hit, raycastDistance, layerMask))
         {
-            Debug.Log("Hit: " + hit.collider.gameObject.name);
-            Debug.DrawLine(origin, hit.point, Color.green);
+            Debug.Log("Disparo");
+            if(hit.collider.gameObject.layer == 6) 
+            {
+                Debug.Log("Hit: " + hit.collider.gameObject.name);
+                Debug.DrawLine(origin, hit.point, Color.green);
+                AppearFluidType1(hit.point);
+            }
+            else if (hit.collider.gameObject.layer == 7)
+            {
+                Debug.Log("Hit: " + hit.collider.gameObject.name);
+                Debug.DrawLine(origin, hit.point, Color.green);
+                AppearFluidType2(hit.point);
+            }
+
+
         }
         else
         {
@@ -39,5 +58,15 @@ public class Shoot : MonoBehaviour
         }
     }
 
+
+    void AppearFluidType1(Vector3 hitPosition)
+    {
+        Instantiate(fluidObjectType1, hitPosition, fluidObjectType1.transform.rotation);
+    }
+
+    void AppearFluidType2(Vector3 hitPosition)
+    {
+        Instantiate(fluidObjectType2, hitPosition, fluidObjectType2.transform.rotation);
+    }
 
 }
